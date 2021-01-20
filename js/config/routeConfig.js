106c26dbf61cf80dbf7b4a6fabcc263104742a69
Caioww <sousinhacaio@gmail.com>
2021-01-20 01:25:37 -0300

    incluindo interceptors para controlar requisições

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -35,6 +35,11 @@ angular.module("listaTelefonica").config(function ($routeProvider, $locationProv
         }
     });
 
+    $routeProvider.when("/error", {
+        templateUrl: "view/error.html",
+
+    });
+
 
     $routeProvider.otherwise({ redirectTo: "/contatos" });
 });
--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,7 +1,18 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, contatos, operadoras, serialGenerator) {
-	$scope.app = "Lista Telefonica";
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, contatos, operadoras, serialGenerator, $filter) {
+	$scope.app = $filter('upper')("Lista Telefonica");
 	$scope.contatos = contatos.data;
 	$scope.operadoras = operadoras.data;
+
+	var init = function () {
+		calcularImpostos($scope.contatos);
+		generateSerial($scope.contatos);
+	};
+
+	var calcularImpostos = function (contatos) {
+		contatos.forEach(function (contato) {
+			contato.operadora.precoComImposto = calcularImposto(contato.operadora.preco);
+		});
+	};
 
 	var generateSerial = function (contatos) {
 		contatos.forEach(function (item) {
@@ -21,9 +32,10 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
 		$scope.contatos = contatos.filter(function (contato) {
 			if (!contato.selecionado) return contato;
 		});
+		$scope.verificarContatoSelecionado($scope.contatos);
 	};
-	$scope.isContatoSelecionado = function (contatos) {
-		return contatos.some(function (contato) {
+	$scope.verificarContatoSelecionado = function (contatos) {
+		$scope.hasContatoSelecionado = contatos.some(function (contato) {
 			return contato.selecionado;
 		});
 	};
@@ -32,5 +44,14 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
 		$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
 	};
 
-	generateSerial($scope.contatos);
+	var calcularImposto = function (preco) {
+		var imposto = 1.2;
+		return preco * imposto;
+	};
+
+	$scope.reset = function () {
+		$scope.contatos = angular.copy($scope.contatos);
+	};
+	
+	init();
 });
